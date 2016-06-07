c63b9eb1cf61e8e1e3b494fb17b10c6d8429dcb5
jorgedavid kugonzalez <jorgedavid kugonzalez>
2016-06-07 11:47:40 -0600

    Proyecto nedbeans

---
+++ src/visordefuentes/Main.java
@@ -0,0 +1,16 @@
+
+package visordefuentes;
+
+import gui.MainFrame;
+
+
+public class Main {
+
+   
+    public static void main(String[] args) {
+       MainFrame ventana= new MainFrame();
+       
+       ventana.setVisible(true);
+    }
+    
+}
